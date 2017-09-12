b158b01b9db494a321305d996a96f46ca2033245
Daman <singhdaman4321@gmail.com>
2017-09-12 20:11:45 +0530

    feat: add game activity

--- app/src/main/java/com/singh/daman/dontmessup/ui/GameActivity.java
+++ app/src/main/java/com/singh/daman/dontmessup/ui/GameActivity.java
@@ -1,17 +1,149 @@
 package com.singh.daman.dontmessup.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.util.ArrayMap;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.singh.daman.dontmessup.R;
+import com.singh.daman.dontmessup.data.Data;
+import com.singh.daman.dontmessup.enums.GestureEnum;
 import com.singh.daman.dontmessup.utils.Utility;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+    private static final String DEBUG_TAG = "Gestures";
+    private GestureDetectorCompat mDetector;
+    private TextView textView;
+    private ArrayMap<String, String> arrayMap;
+    private String[] s = new String[2];
+    private Handler handler = new Handler();
+    private int progress = 0;
+    private ProgressBar progressBar;
+    private boolean click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Utility.setFullScreen(this);
         setContentView(R.layout.activity_game);
+        mDetector = new GestureDetectorCompat(this, this);
+        mDetector.setOnDoubleTapListener(this);
+        textView = (TextView) findViewById(R.id.text_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        arrayMap = Data.createArrayMap();
+        s = Data.getData(arrayMap);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(s[0]);
+                loop();
+            }
+        },500);
+
+    }
+
+    Runnable updateTime = new Runnable() {
+        public void run() {
+            handler.postDelayed(this, 20);
+            progress = progress + 2;
+            progressBar.setProgress(progress);
+            if(progress == 100){
+                s = Data.getData(arrayMap);
+                textView.setText(s[0]);
+                progress = 0;
+                if(click) {
+                    loop();
+                    click = false;
+                }
+                else {
+                    handler.removeCallbacks(this);
+                    Toast.makeText(GameActivity.this, "Out", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+
+        }
+    };
+
+    private void loop() {
+        progressBar.setProgress(progress);
+        handler.removeCallbacks(updateTime);
+        handler.postDelayed(updateTime, 100);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        Log.d(DEBUG_TAG, "onFling: " + event1.getAction());
+        check(GestureEnum.SWIPE.toString());
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent event1, MotionEvent event2, float distanceX,
+                            float distanceY) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onDoubleTap: " + event.getAction());
+        check(GestureEnum.DOUBLETAP.toString());
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: " + event.getAction());
+        check(GestureEnum.TAP.toString());
+        return true;
+    }
+
+    private void check(String value) {
+        click = true;
+        if (!s[1].equals(value)) {
+            handler.removeCallbacks(updateTime);
+            Toast.makeText(this, "OUT", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/singh/daman/dontmessup/ui/MenuActivity.java
@@ -0,0 +1,26 @@
+package com.singh.daman.dontmessup.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.singh.daman.dontmessup.R;
+
+public class MenuActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MenuActivity.this, GameActivity.class));
+            }
+        });
+    }
+}
