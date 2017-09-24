b4b9b023c29d100f9a4d9332790666421c05f0a2
Daman <singhdaman4321@gmail.com>
2017-09-24 17:24:04 +0530

    feat: add animation on game start

--- app/src/main/java/com/singh/daman/dontmessup/ui/MenuActivity.java
+++ app/src/main/java/com/singh/daman/dontmessup/ui/MenuActivity.java
@@ -1,23 +1,39 @@
 package com.singh.daman.dontmessup.ui;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.singh.daman.dontmessup.R;
 import com.singh.daman.dontmessup.utils.Utility;
 
 public class MenuActivity extends AppCompatActivity {
     private Button button;
+    private TextView textView;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Utility.setFullScreen(this);
         setContentView(R.layout.activity_menu);
+        linearLayout = (LinearLayout) findViewById(R.id.screen);
         button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.text_app_name);
+
+        ObjectAnimator colorFade = ObjectAnimator.ofObject(linearLayout, "backgroundColor", new ArgbEvaluator(), ContextCompat.getColor(this, R.color.colorPrimary), ContextCompat.getColor(this, R.color.colorAccent));
+        colorFade.setDuration(3500);
+        colorFade.setStartDelay(200);
+        colorFade.start();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/singh/daman/dontmessup/ui/SplashActivity.java
+++ app/src/main/java/com/singh/daman/dontmessup/ui/SplashActivity.java
@@ -1,20 +1,26 @@
 package com.singh.daman.dontmessup.ui;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.singh.daman.dontmessup.R;
 import com.singh.daman.dontmessup.utils.Utility;
 
 public class SplashActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Utility.setFullScreen(this);
         setContentView(R.layout.activity_splash);
+        textView = (TextView) findViewById(R.id.text_app_name);
         Handler handler = new Handler();
         handler.postDelayed(runnable, 3000);
     }
@@ -23,7 +29,14 @@ public class SplashActivity extends AppCompatActivity {
         @Override
         public void run() {
             Intent intent = new Intent(SplashActivity.this, MenuActivity.class);
-            startActivity(intent);
+            ActivityOptionsCompat options = ActivityOptionsCompat.
+                    makeSceneTransitionAnimation(SplashActivity.this, textView, getString(R.string.shared_textview));
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                startActivity(intent, options.toBundle());
+            } else{
+                startActivity(intent);
+            }
+//            overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
         }
     };
 }
